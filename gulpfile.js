0a87fe47996bcd063b2d6aba89a0e0d5f8f90eea
Diego Nieto Cid <dnietoc@gmail.com>
2016-10-25 12:35:28 -0300

    Wrap forge bundle in a module.exports setting function
    ...to appease Meteor.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var rename = require('gulp-rename');
 var clean  = require('gulp-clean');
 var run = require('gulp-run');
 var replace = require('gulp-replace');
+var wrap = require('gulp-wrap');
 
 gulp.task('clean-dist', function () {
 	return gulp.src(['./dist', './tmp'], {read: false})
@@ -27,6 +28,7 @@ gulp.task('install-forge', ['bundle-forge'], function () {
 			.pipe(replace(/(.*factory\()require(,.*)/g,"$1require_internal$2"))
 			.pipe(replace(/(.*function\()require(,.*)/g,"$1require_internal$2"))
 			//.pipe(replace(/(.*)require(\('crypto.*)/g, "$1require_internal$2"))
+			.pipe(wrap({src: 'src/forge.js.template'}))
 			.pipe(rename('forge.js'))
 			.pipe(gulp.dest('./dist'));
 });
